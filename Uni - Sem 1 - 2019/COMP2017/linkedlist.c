4e3a46f6ff472a37ee72ec77cd244b288b91b6ad
Aidan Peruch <aper9336@gmail.com>
2019-04-05 10:55:33 +1100

    added linked list changes

--- Uni - Sem 1 - 2019/COMP2017/linkedlist.c
+++ Uni - Sem 1 - 2019/COMP2017/linkedlist.c
@@ -1,40 +1,42 @@
 #include<stdio.h>
 #include<stdlib.h>
 
-struct node {
-    int value;
-    struct node * next;
-} n;
+/*
+list_init
+Creates a new list by creating and head node
+:: int value :: The value to be stored in the head node
+Returns a pointer to the newly created node
+*/
+struct node* list_init(int value);
 
-struct curr {
-    struct node * curr;
-    struct node * next;
-    struct node * prev;
-    struct node * head;
-    struct node * tail;
-};
+/*
+list_add
+Adds a node containing a specified value to an existing list
+:: int value :: The value to be stored in the new node
+Does not return anything
+*/
+void list_add(struct node* head, int value);
 
-struct node* list_init(int value){
-    struct node * newHead;
-    newHead = (struct node *)malloc(sizeof(n));
-    newHead->value = value;
-    newHead->next = NULL;
-    return newHead;
-}
+/*
+list_delete
+Removes the specified node from the list and updates the list accordingly
+:: struct node* n :: The pointer to the node to be deleted
+Does not return anything
+*/
+void list_delete(struct node** head, struct node* n);
 
-struct node* list_add(struct node* head, int value){
-    struct node * newNode;
-    newNode = (struct node *)malloc(sizeof(n));
-    while(head->next != NULL){
-        head = head -> next;
-    }
-    newNode->value = value;
-    newNode->next = NULL;
-}
+/*
+list_next
+Returns a pointer to the next node in the list
+:: const struct node* n :: The node
+Returns a pointer to the next node
+*/
+struct node* list_next(const struct node* n);
 
-int main(void){
-    struct node * testHead = list_init(33);
-    printf("newHead is: %p\n", testHead);
-    printf("newHead->value is: %d\n", testHead->value);
-    printf("newHead->next is: %p\n", testHead->next);
-}
+/*
+list_free
+Frees all existing nodes in a list
+:: struct node* n :: The pointer to the head node of the list
+Does not return anything
+*/
+void list_free(struct node* head);
